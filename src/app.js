80be526d06c7c2f66c15aecbece54390cf5d36dd
ITokianaR <tokina.raman@gmail.com>
2026-02-01 17:35:03 +0300

    feat : adding database connection feature

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import databaseConnection from "./config/database.js";
+import { configDotenv } from "dotenv";
 
+configDotenv();
+await databaseConnection();
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json());
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
