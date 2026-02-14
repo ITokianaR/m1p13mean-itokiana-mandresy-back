21ef223e186dd631face940049f241677e40b18b
ITokianaR <tokina.raman@gmail.com>
2026-02-14 19:32:53 +0300

    fix: naming typos for middleware

--- src/middlewares/authorizeRole.js
+++ src/middlewares/authorizeRole.js
@@ -1,7 +1,7 @@
 const authorizeRoles = (...allowredRoles) => {
     return (req, res, next) => {
         if (!req.user || !allowredRoles.includes(req.user.role)) {
-            return res.status(403).json({ message: "Access denied" });
+            return res.status(403).json({ message: `Access denied. You are a ${req.user.role}` });
         }
         next();
     }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
-import verifyToken from "../middlewares/auth.middleware.js";
-import authorizeRoles from "../middlewares/role.middleware.js";
+import verifyToken from "../middlewares/verifyToken.js";
+import authorizeRoles from "../middlewares/authorizeRole.js";
 
 const router = Router();
 
