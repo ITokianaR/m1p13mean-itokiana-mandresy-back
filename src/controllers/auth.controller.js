d95d98f9f8f6e5a2464495fd543e80d1220304fd
ITokianaR <tokina.raman@gmail.com>
2026-02-14 15:05:35 +0300

    feat: login function ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,30 @@
-import { signUp }  from '../services/auth.service.js';
+import { signUp, signIn }  from '../services/auth.service.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post(
     '/user/signup', async (req, res, next) => {
-    try{
-        const user = await signUp(req.body);
-        res.json({ user });
-    } catch (error) {
-        next(error);
+        try{
+            const user = await signUp(req.body);
+            res.json({ user });
+        } catch (error) {
+            next(error);
+        }
     }
-});
+);
+
+
+router.post(
+    '/user/signin', async (req, res, next) => {
+        try{
+            const userLogged = await signIn(req.body);
+            res.json({ userLogged })
+        } catch (error) {
+            next(error)
+        }
+    }
+)
+
 
 export default router;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,12 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import JWT from 'jsonwebtoken';
 
 export const signUp = async (input) => {
     if (!input || !input.email || !input.username || !input.password || !input.fullname) {
-        throw new Error("Invalid input data");
+        const error = new Error("Invalid input data");
+        error.status = 400;
+        throw error;
     }
 
     const username = input.username;
@@ -14,6 +16,20 @@ export const signUp = async (input) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+        const error = new Error("Email already exists");
+        error.status = 409;
+        throw error;
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+        const error = new Error("Username already exists");
+        error.status = 409;
+        throw error;
+    }
+
     const user = await User.create({
         username: username,
         fullname: fullname,
@@ -22,4 +38,50 @@ export const signUp = async (input) => {
     });
 
     return user;
+}
+
+export const signIn = async (input) => {
+    const email = input.email;
+    const password = input.password;
+
+    if (!email) {
+        const error = new Error("Email can't be blank");
+        error.status = 400;
+        throw error;
+    }
+
+    if (!password) {
+        const error = new Error("Password can't be blank");
+        error.status = 400;
+        throw error;
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        const error = new Error(`User with email: ${email} was not found.`);
+        error.status = 404;
+        throw error;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        const error = new Error("Invalid credentials");
+        error.status = 400;
+        throw error;
+    }
+
+    const token = JWT.sign(
+        { id: user._id, role:user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    )
+    
+    return {
+        token,
+        user: {
+            email: user.email,
+            username: user.username
+        }
+    };
 }
