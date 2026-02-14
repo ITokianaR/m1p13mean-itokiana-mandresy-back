426f6b693b5365645dca3a83c8aff1b6e0cd2149
ITokianaR <tokina.raman@gmail.com>
2026-02-14 16:22:50 +0300

    chores: add another user route

---
+++ src/routes/userRoute.js
@@ -0,0 +1,22 @@
+import express, { Router } from "express";
+import verifyToken from "../middlewares/auth.middleware.js";
+import authorizeRoles from "../middlewares/role.middleware.js";
+
+const router = Router();
+
+// admin route
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
+    res.json({ message: "Welcome admin" });
+});
+
+// shop route
+router.get("/shop", verifyToken, authorizeRoles("admin", "shop"), (req, res) => {
+    res.json({ message: "Welcome shop" });
+});
+
+// user route
+router.get("/client", verifyToken, authorizeRoles("admin", "client"), (req, res) => {
+    res.json({ message: "Welcome client" });
+});
+
+export default router;
